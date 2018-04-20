e1425ef50681ae041f398935f27fecd3bd380611
Blake Thomson <thomsbg@gmail.com>
2018-04-20 10:18:22 -0700

    build

--- dist/quill.core.js
+++ dist/quill.core.js
@@ -2715,15 +2715,6 @@ function normalizeDelta(delta) {
       delete attributes['image'];
       return delta.insert({ image: op.attributes.image }, attributes);
     }
-    if (op.attributes != null && (op.attributes.list === true || op.attributes.bullet === true)) {
-      op = (0, _clone2.default)(op);
-      if (op.attributes.list) {
-        op.attributes.list = 'ordered';
-      } else {
-        op.attributes.list = 'bullet';
-        delete op.attributes.bullet;
-      }
-    }
     if (typeof op.insert === 'string') {
       var text = op.insert.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
       return delta.insert(text, op.attributes);
